6a533d52f5629bc929cb387265e35ff08c6dd6cd
tommygraven <tommygraven@me.com>
2018-01-27 17:12:01 +0000

    multiple Portal Door function

--- Assets/Scripts/EnergyObject.cs
+++ Assets/Scripts/EnergyObject.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnergyObject : MonoBehaviour {
-	public bool isVisible = true;
-
+	public bool isVisible = true; 
 
 
 	// Use this for initialization
@@ -15,20 +14,40 @@ public class EnergyObject : MonoBehaviour {
 
 			GetComponent<Collider>().isTrigger = true;
 		}
-			
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+		
+	bool ParentEnergy()
+	{
+		Transform parent = transform.parent;
+
+		if (parent == null)
+			return false;
+			
+		EnergyObjectList energyObjectList = parent.GetComponent<EnergyObjectList>();
+		if (energyObjectList == null)
+			return false;
+
+
+		energyObjectList.SwitchState(isVisible);
+		return true;
+	}
+
+
 
 	public void SwitchState()
 	{
+		if (ParentEnergy())
+			return;
+
 		isVisible = !isVisible;
 		GetComponent<Renderer>().enabled = isVisible;
 
 		gameObject.GetComponent<Collider>().isTrigger = !isVisible;
-
+		print("regular switch state");
 	}
 }
---
+++ Assets/Scripts/EnergyObjectList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyObjectList : MonoBehaviour {
+
+	GameObject[] energyGameObjects;
+	public bool isVisible;
+
+
+
+	// Use this for initialization
+	void Start () {
+		int childrenCount = transform.childCount;
+		energyGameObjects = new GameObject[childrenCount];
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SwitchState(bool _isVisible)
+	{
+		_isVisible = !_isVisible;
+		isVisible = _isVisible;
+		for (int i = 0; i < energyGameObjects.Length; i++)
+		{
+			transform.GetChild(i).GetComponent<EnergyObject>().isVisible = isVisible;
+			transform.GetChild(i).GetComponent<Renderer>().enabled = isVisible;
+			transform.GetChild(i).GetComponent<Collider>().isTrigger = !isVisible;
+			print("Object " + i + " renderer is " + isVisible);
+			print("Object " + i + " collider is " + !isVisible);
+		}
+	}
+
+}
