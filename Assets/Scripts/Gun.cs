514a37e4039371f3b0d4c2b238b12e7a8bffb2b4
tommygraven <tommygraven@me.com>
2018-01-27 01:28:35 +0000

    Basic Gun Functionality
    You can make special objects disappear and appear by looking at them
    and pressing down your mouse button.
    The script also keeps track of when the gun actually has energy

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+	Camera playerCam;
+	bool isLoaded;
+
+	void Awake() {
+		playerCam = GetComponent<Camera>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject raycastedObj = RaycastObject();
+		if (raycastedObj == null)
+			return;
+		
+		EnergyObject raycastedEnergyObj = raycastedObj.GetComponent<EnergyObject>();
+		if (raycastedEnergyObj == null)
+			return;
+
+		if (raycastedEnergyObj.isVisible)
+		{
+			Absorb();
+			raycastedEnergyObj.SwitchState();
+		}
+
+		else if (isLoaded && !raycastedEnergyObj.isVisible)
+		{
+			Shoot();
+			raycastedEnergyObj.SwitchState();
+		}
+	}
+
+	GameObject RaycastObject()
+	{
+		RaycastHit hit;
+		float dist;
+
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+		Debug.DrawRay(transform.position, forward, Color.green);
+
+		if(Physics.Raycast(transform.position, forward, out hit))
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				print(hit.transform.gameObject.name);
+				return hit.transform.gameObject;
+			}
+		}
+
+		return null;
+	}
+
+	void Absorb()
+	{
+		isLoaded = true;
+	}
+
+	void Shoot()
+	{
+		isLoaded = false;
+	}
+}
