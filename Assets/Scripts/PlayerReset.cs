d50c72b27f682d148f068e9089e9f57eb94f94d1
Wichaya Karnchanapee <wkarnchanapee@gmail.com>
2018-01-28 12:08:40 +0000

    Level Reset

---
+++ Assets/Scripts/PlayerReset.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerReset : MonoBehaviour {
+
+    private Vector3 startPos;
+    [SerializeField] KeyCode hotKey = KeyCode.R;
+    public UnityEvent resetBtnPressed;
+   
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+        if (resetBtnPressed == null)
+        {
+            resetBtnPressed = new UnityEvent();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(hotKey))
+        {
+            resetBtnPressed.Invoke();
+        }
+	}
+
+    public void ResetPosition()
+    {
+        Debug.Log("Reset Position Called!");
+    }
+    
+}
